2b230691c289b02a4321281d1f336c551abc0ffc
Michael0669 <Michaelhampshire917@gmail.com>
2025-05-04 18:24:41 -0400

    Add project files.

---
+++ InheritanceMiniProject/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceMiniProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<InventoryItemModel> inventory = new List<InventoryItemModel>();
+
+            inventory.Add(new VehicleModel { DealerFee = 25, ProductName = "Kia" });
+            inventory.Add(new BookModel { ProductName = "Harry Potter", NumberOfPages = 332 });
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IRentable
+    {
+        void Rent();
+        void ReturnRental();
+    }
+
+    public interface IPurchasable
+    {
+        void Purchase();
+    }
+    public class InventoryItemModel
+    {
+        public string ProductName { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+
+    public class VehicleModel : InventoryItemModel
+    {
+        public decimal DealerFee { get; set; }
+    }
+
+    public class BookModel : InventoryItemModel, IPurchasable
+    {
+        public int NumberOfPages { get; set; }
+
+        public void Purchase()
+        {
+            QuantityInStock -= 1;
+            Console.WriteLine("This book has been purchased.");
+        }
+    }
+
+    public class Excavator : InventoryItemModel, IRentable
+    {
+        public void Dig()
+        {
+            Console.WriteLine("I am digging");
+        }
+
+        public void Rent()
+        {
+            QuantityInStock -= 1;
+            Console.WriteLine("This excavator was rented");
+        }
+
+        public void ReturnRental()
+        {
+            QuantityInStock += 1;
+            Console.WriteLine("This excavator was returned");
+        }
+    }
+}
