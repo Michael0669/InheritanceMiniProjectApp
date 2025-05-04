be29dd53a6a2653009075b49788ccecdba54b76b
Michael0669 <Michaelhampshire917@gmail.com>
2025-05-04 18:58:00 -0400

    Added conditional statement asking user to either rent or purchase items from lists.

--- InheritanceMiniProject/Program.cs
+++ InheritanceMiniProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,21 +12,80 @@ namespace InheritanceMiniProject
         static void Main(string[] args)
         {
             List<InventoryItemModel> inventory = new List<InventoryItemModel>();
+            List<IPurchasable> purchasables = new List<IPurchasable>();
+            List<IRentable> rentables = new List<IRentable>();
 
-            inventory.Add(new VehicleModel { DealerFee = 25, ProductName = "Kia" });
-            inventory.Add(new BookModel { ProductName = "Harry Potter", NumberOfPages = 332 });
+            var vehicle = new VehicleModel { DealerFee = 25, ProductName = "Kia" };
+            var book = new BookModel { ProductName = "Harry Potter", NumberOfPages = 332 };
+            var excavator = new ExcavatorModel { ProductName = "Excavator", QuantityInStock = 2 };
 
-            Console.ReadLine();
+            purchasables.Add(vehicle);
+            purchasables.Add(book);
+
+            rentables.Add(vehicle);
+            rentables.Add(excavator);
+
+            Console.WriteLine("Do you want to rent or purchase something: ");
+            string rentalDecision = Console.ReadLine();
+
+            if (rentalDecision.ToLower() == "rent")
+            {
+                foreach (var item in rentables)
+                {
+                    Console.WriteLine($"Item {item.ProductName}");
+                    Console.Write("Do you want to rent this item: ");
+                    string wantToRent = Console.ReadLine();
+
+                    if (wantToRent.ToLower() == "yes")
+                    {
+                        item.Rent();
+                    }
+
+                    Console.WriteLine("Do you want to return this item: ");
+                    string wantToReturn = Console.ReadLine();
+
+                    if (wantToReturn.ToLower() == "yes")
+                    {
+                        item.ReturnRental();
+                    }
+                }
+            }
+            else
+            {
+                foreach (var item in purchasables)
+                {
+                    Console.WriteLine($"Item {item.ProductName}");
+                    Console.Write("Do you want to purchase this item: ");
+                    string wantToPurchase = Console.ReadLine();
+
+                    if (wantToPurchase.ToLower() == "yes")
+                    {
+                        item.Purchase();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thank you, come again.");
+                    }
+                    
+                }
+            }
+
+                Console.ReadLine();
         }
     }
 
-    public interface IRentable
+    public interface IInventoryItem
+    {
+        string ProductName { get; set; }
+        int QuantityInStock { get; set; }
+    }
+    public interface IRentable : IInventoryItem
     {
         void Rent();
         void ReturnRental();
     }
 
-    public interface IPurchasable
+    public interface IPurchasable : IInventoryItem
     {
         void Purchase();
     }
@@ -69,7 +129,7 @@ namespace InheritanceMiniProject
         }
     }
 
-    public class Excavator : InventoryItemModel, IRentable
+    public class ExcavatorModel : InventoryItemModel, IRentable
     {
         public void Dig()
         {
